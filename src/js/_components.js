db5fecf53413367651bda5dce7c2a457df587040
Serezha <volos11223344@yandex.ru>
2024-05-25 07:44:42 +0300

    подключает стилизует slider swiper

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,36 @@
-console.log('components');
+console.log("components");
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true,
+  autoplay: {
+    delay: 3000, // Время (в миллисекундах) между переключениями слайдов
+    disableOnInteraction: false, // Продолжать автоматическое проигрывание после взаимодействия пользователя
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: "auto",
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+});
+
+console.log(swiper);

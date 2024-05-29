4c86ff855fd48ffdaa25c72590151c00be6d3010
s.potapov <s.potapov@severnaya.ru>
2024-05-29 18:11:34 +0300

    добавляет галерею

--- src/js/_components.js
+++ src/js/_components.js
@@ -5,16 +5,3 @@ import "./components/animation-page.js";
 //Вызовы функций
 openCloseAccordion();
 initSwiper();
-
-// const trigger = new ScrollTrigger();
-// // Add all html elements with attribute data-trigger
-// trigger.add("[data-trigger]");
-
-// Now in your CSS add the following classes, this fades the [data-trigger] elements in and out
-// .visible, .invisible {
-//   opacity: 0.0;
-//   transition: opacity 0.5s ease;
-// }
-// .visible {
-//   opacity: 1.0;
-// }
--- src/js/components/animation-page.js
+++ src/js/components/animation-page.js
@@ -10,11 +10,26 @@ const sr = ScrollReveal();
 // });
 
 const list = document.querySelector(".qeustions__list");
+const formBox = document.querySelector(".login-box");
 
 sr.reveal(list, {
   duration: 1000, // Длительность анимации в миллисекундах
   distance: "70px", // Расстояние, на которое элемент должен сдвинуться
   origin: "left", // Направление анимации (top, bottom, left, right)
+  delay: 200, // Задержка перед началом анимации (в миллисекундах)
+  // opacity: null,
+  reset: true, // Повторение анимации при повторном появлении элемента
+  viewport: {
+    // Опция для анимации при появлении в viewport
+    global: true, // Применять ко всем элементам на странице
+    amount: 0.25, // Доля видимости элемента в viewport, при которой происходит анимация (0-1)
+  },
+});
+
+sr.reveal(formBox, {
+  duration: 1000, // Длительность анимации в миллисекундах
+  distance: "70px", // Расстояние, на которое элемент должен сдвинуться
+  origin: "right", // Направление анимации (top, bottom, left, right)
   delay: 200, // Задержка перед началом анимации (в миллисекундах)
   // opacity: null,
   reset: true, // Повторение анимации при повторном появлении элемента

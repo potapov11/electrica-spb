a2afebe08285adad037784988a929f11547f9c20
s.potapov <s.potapov@severnaya.ru>
2024-05-31 09:31:39 +0300

    добавляет моб форму

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,6 +2,75 @@ import { openCloseAccordion } from "./components/open-close-accordion.js";
 import { initSwiper } from "./components/init-swiper.js";
 import "./components/animation-page.js";
 
+const btnPrice = document.querySelector(".header__price");
+const modalForm = document.querySelector(".modal-form");
+const btnClose = document.querySelector(".modal__close");
+
+btnPrice.addEventListener("click", () => {
+  console.log("modal__inactive");
+  modalForm.classList.remove("modal__inactive");
+  modalForm.style = "";
+});
+
+btnClose.addEventListener("click", () => {
+  console.log("modal__inactive");
+  modalForm.classList.add("modal__inactive");
+});
+
+const modal = document.querySelector(".modal");
+const modalHeader = document.querySelector(".modal__header");
+const modalSpan = modalHeader.querySelector("span");
+
+let isDragging = false;
+let currentY;
+let initialY;
+let currentTranslate = 0;
+let initialTranslate = 0;
+let currentOpacity = 1;
+let currentHeight = 100;
+
+modalHeader.addEventListener("touchstart", dragStart);
+modalHeader.addEventListener("touchend", dragEnd);
+modalHeader.addEventListener("touchmove", drag);
+
+function dragStart(e) {
+  initialY = e.touches[0].clientY - currentTranslate;
+  isDragging = true;
+}
+
+function dragEnd(e) {
+  initialTranslate = currentTranslate;
+
+  if (Math.abs(currentY - initialY) > 100) {
+    modal.classList.add("modal__inactive");
+  } else {
+    modal.style.height = "100%";
+  }
+
+  isDragging = false;
+}
+
+function drag(e) {
+  if (isDragging) {
+    e.preventDefault();
+
+    currentY = e.touches[0].clientY;
+    currentTranslate = currentY - initialY;
+
+    modal.style.transform = `translate(0, ${currentTranslate}px)`;
+    currentOpacity = 1 - Math.abs(currentTranslate) / 100;
+    modal.style.opacity = currentOpacity;
+
+    currentHeight = 100 - Math.abs(currentTranslate) / 2;
+    modal.style.height = `${currentHeight}%`;
+  }
+}
+
+// В dragStart запоминаем начальную координату Y касания.
+// В drag обновляем позицию модального окна по вертикали и его высоту в процентах. Высота будет уменьшаться пропорционально перемещению вниз, достигая 50% при перемещении на 100 пикселей.
+// В dragEnd проверяем, если перемещение по оси Y больше 100 пикселей, то добавляем класс modal__inactive, чтобы закрыть модальное окно. Иначе возвращаем высоту окна к 100%.
+// Таким образом, при перетаскивании шапки модального окна вниз, высота окна будет плавно уменьшаться, а при отпускании, если перемещение было достаточно большим, окно закроется.
+
 //Вызовы функций
 openCloseAccordion();
 initSwiper();
